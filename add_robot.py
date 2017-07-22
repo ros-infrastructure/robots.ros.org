0c63efefdaf712c9597c843c653f3ac7efe6ac0e
wzy0426 <2198447575@qq.com>
2017-07-23 01:51:33 +0800

    Add Bulldog Robot

--- add_robot.py
+++ add_robot.py
@@ -14,36 +14,36 @@ d = {}
 today = date.today()
 d['date_string'] = today.strftime('%Y-%m-%d')
 while True:
-    d['title'] = raw_input("Please enter the robots name to be the title: ")
+    d['title'] = nput("Please enter the robots name to be the title: ")
     if d['title']:
         break
 d['title_filename'] = d['title'].lower().replace(' ', '-')
 while True:
-    d['short_description'] = raw_input("Please enter a one or two sentance description of the robot: ")
+    d['short_description'] = input("Please enter a one or two sentance description of the robot: ")
     if d['short_description']:
         break
 d['tags'] = []
 while True:
-    response = raw_input("Please enter a tag for this robot. (You will be prompted again until you enter a tag 'done': ")
+    response = input("Please enter a tag for this robot. (You will be prompted again until you enter a tag 'done': ")
     if response == 'done':
         break
     if not response:
         continue
     d['tags'].append(response)
 while True:
-    response = raw_input("Please enter the category for this robot from %s: " % valid_categories)
+    response = input("Please enter the category for this robot from %s: " % valid_categories)
     if response in valid_categories:
         d['main_class'] = response
         break
-d['wiki_homepage'] = raw_input("Please enter the wiki homepage url for this robot: ")
-d['website'] = raw_input("Please enter the website url for this robot (optional): ")
-d['color'] = raw_input("Please pick a color for this robot's page. (#RRGGBB hexadecimal with the '#'): ")
+d['wiki_homepage'] = input("Please enter the wiki homepage url for this robot: ")
+d['website'] = input("Please enter the website url for this robot (optional): ")
+d['color'] = input("Please pick a color for this robot's page. (#RRGGBB hexadecimal with the '#'): ")
 image_folder = os.path.abspath('assets/img/robots/' + d['title_filename'])
 if not os.path.exists(image_folder):
     os.makedirs(image_folder)
 print("Resources for this post should be placed in the folder %s" % (image_folder))
-d['icon_filename'] = raw_input("Please enter the name of the icon (80px optimal) filename relative to the above path such as icon.png: ")
-d['image_filename'] = raw_input("Please enter the name of the image (600px optimal) filename relative to the above path such as image.png: ")
+d['icon_filename'] = input("Please enter the name of the icon (80px optimal) filename relative to the above path such as icon.png: ")
+d['image_filename'] = input("Please enter the name of the image (600px optimal) filename relative to the above path such as image.png: ")
 
 
 
@@ -56,7 +56,7 @@ print("Content is:")
 pp = pprint.PrettyPrinter(indent=4)
 pp.pprint(d)
 
-response = raw_input("Does this look correct? [y/N]: ")
+response = input("Does this look correct? [y/N]: ")
 if not response.startswith('y'):
     if os.path.exists(image_folder):
         print("the assets directory was already created, you may want to delete it: " + image_folder)
