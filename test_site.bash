ca7cb87556fd77702c9bc98cf439772e36e07593
Tully Foote <tfoote@osrfoundation.org>
2018-05-01 19:03:42 -0700

    Switch to https by default.
    https://blog.github.com/2018-05-01-github-pages-custom-domains-https/

--- test_site.bash
+++ test_site.bash
@@ -4,5 +4,5 @@ set -x
 set -o errexit
 
 docker build -t "robotssite" .
-sed 's|http://robots.ros.org|http://localhost:3000|' _config.yml > localtest_config.yml
+sed 's|https://robots.ros.org|http://localhost:3000|' _config.yml > localtest_config.yml
 docker run -v `pwd`:/tmp/jekyll -v `pwd`/localtest_config.yml:/tmp/jekyll/_config.yml -w /tmp/jekyll -i -t --rm --net=host robotssite
